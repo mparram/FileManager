08fcad2d077303e84202b1f670043d1499414e31
PARRA DE MIGUEL Mario <mario.parra@soprasteria.com>
2020-04-06 17:18:49 +0200

    added kafka integration

--- kafka.js
+++ kafka.js
@@ -5,36 +5,37 @@ var kafka = require('kafka-node');
 var Producer = kafka.Producer;
 var client = new kafka.KafkaClient({kafkaHost: kafka_server + ":" + kafka_port});
 var producer = new Producer(client);
-
+var dbmodules = module.exports = {
+    writefile: function(id, url, filename, size, ext, uploadDate, encoding, mimetype) {
+        let message = [
+            {
+                id: id,
+                url: url,
+                filename: filename,
+                size: size,
+                ext: ext, 
+                uploadDate: uploadDate,
+                uploadDate: uploadDate,
+                mimetype: mimetype
+            }
+        ];
+        let payloads = [
+            {
+              topic: kafka_topic,
+              messages: message
+            }
+          ];
+        producer.send(payloads, function (err, data) {
+            if (err) {
+                console.log(err);
+            };
+            console.log(data);
+            console.log("added message to kafka topic: " + kafka_topic);
+            console.log("message: ");
+            console.dir(message);
+        });
+    }
+};
 producer.on('ready', function () {
-    var dbmodules = module.exports = {
-        writefile: function(id, url, filename, size, ext, uploadDate, encoding, mimetype) {
-            let message = [
-                {
-                    id: id,
-                    url: url,
-                    filename: filename,
-                    size: size,
-                    ext: ext, 
-                    uploadDate: uploadDate,
-                    uploadDate: uploadDate,
-                    mimetype: mimetype
-                }
-            ];
-            let payloads = [
-                {
-                  topic: kafka_topic,
-                  messages: message
-                }
-              ];
-            producer.send(payloads, function (err, data) {
-                if (err) {
-                    console.log(err);
-                };
-                console.log(data);
-                console.log("added message to kafka topic: " + kafka_topic);
-                console.log("message: " + message);
-            });
-        }
-    };
+    
 });
